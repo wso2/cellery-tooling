7fbcfb1bcd3d20b800b080d9489120db2033761b
NadunDeSilva <nadunrds@gmail.com>
2019-11-03 20:10:44 +0530

    Add support to sync Image Manager with local repository

--- components/blangserver-plugins/src/main/java/io/cellery/tooling/ballerina/langserver/plugins/ImageManager.java
+++ components/blangserver-plugins/src/main/java/io/cellery/tooling/ballerina/langserver/plugins/ImageManager.java
@@ -32,8 +32,9 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.zip.ZipEntry;
@@ -43,10 +44,11 @@ import java.util.zip.ZipFile;
  * Cellery Image Manager for fetching and extracting information from Images.
  */
 public class ImageManager {
+    private static final Logger logger = LoggerFactory.getLogger(ImageManager.class);
     private static final Executor executor = Executors.newWorkStealingPool();
-    private static final Gson gson = new Gson();
     private static final ImageManager instance = new ImageManager();
-    private final Map<String, Image> images = new ConcurrentHashMap<>();
+
+    private final Map<String, Image> images = new HashMap<>();
 
     public static ImageManager getInstance() {
         return instance;
@@ -54,22 +56,146 @@ public class ImageManager {
 
     /**
      * Get a Cellery image.
+     *
      * If the image does not exist in the local repository it will be automatically pulled.
      *
      * @param orgName The name of the organization the image belongs to
      * @param imageName The name of the image
      * @param version The version of the image
-     * @return The image from which information can be extracted
+     * @return The image from which information had be extracted
      */
-    public Image getImage(String orgName, String imageName, String version) {
-        String imageFQN = orgName + "/" + imageName + ":" + version;
-        Image image = images.computeIfAbsent(imageFQN, k -> new Image(orgName, imageName, version));
+    public synchronized Image getImage(String orgName, String imageName, String version) {
+        String imageFQN = getImageFQN(orgName, imageName, version);
+        Image image = images.get(imageFQN);
 
         // Ensuring that the actual image and the last image of which information was collected is equal
-        if (!Arrays.equals(image.getLastKnownDigest(), image.getCurrentDigest())) {
-            image.extractInformationFromImage();
+        if (image == null) {
+            File imageFile = new File(Constants.LOCAL_REPO_DIRECTORY + File.separator + orgName
+                    + File.separator + imageName + File.separator + version + File.separator
+                    + imageName + Constants.CELLERY_IMAGE_EXTENSION);
+            if (imageFile.exists()) {
+                image = initializeImageFromLocalRepo(orgName, imageName, version);
+            } else {
+                pullImage(orgName, imageName, version);
+            }
+        } else if (image.getCurrentDigest() == null) {  // Previous image had been deleted
+            images.remove(imageFQN);
+            pullImage(orgName, imageName, version);
+        } else if (!Arrays.equals(image.getCurrentDigest(), image.getLastKnownDigest())) {
+            image.extractInformation();
         }
         return image;
+    }
+
+    /**
+     * Get the images collections currently kept in memory.
+     *
+     * @return The images collection.
+     */
+    public synchronized Collection<Image> getImages() {
+        syncWithLocalRepo();
+        return images.values();
+    }
+
+    /**
+     * Start a thread which periodically checks the local repository for changes.
+     */
+    private void syncWithLocalRepo() {
+        for (Map.Entry<String, Image> imageEntry : images.entrySet()) {
+            if (imageEntry.getValue().getCurrentDigest() == null) {
+                images.remove(imageEntry.getKey());
+            }
+        }
+
+        File localRepo = new File(Constants.LOCAL_REPO_DIRECTORY);
+        File[] orgDirectories = localRepo.listFiles();
+        if (orgDirectories != null) {
+            // Looping organizations
+            for (File orgDirectory : orgDirectories) {
+                File[] imageDirectories = orgDirectory.listFiles();
+                if (imageDirectories != null) {
+                    // Looping images in an organization
+                    for (File imageDirectory : imageDirectories) {
+                        File[] versionDirectories = imageDirectory.listFiles();
+                        if (versionDirectories != null) {
+                            // Looping versions in a image
+                            for (File versionDirectory : versionDirectories) {
+                                if (versionDirectory.exists() && versionDirectory.isDirectory()) {
+                                    executor.execute(() -> {
+                                        String org = orgDirectory.getName();
+                                        String name = imageDirectory.getName();
+                                        String version = versionDirectory.getName();
+
+                                        synchronized (this) {
+                                            Image image = images.get(getImageFQN(org, name, version));
+                                            if (image == null) {
+                                                initializeImageFromLocalRepo(org, name, version);
+                                            } else if (!Arrays.equals(image.getCurrentDigest(),
+                                                    image.getLastKnownDigest())) {
+                                                image.extractInformation();
+                                            }
+                                        }
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Pull Image from remote registry.
+     *
+     * @param orgName The name of the organization the image belongs to
+     * @param imageName The name of the image
+     * @param version The version of the image
+     */
+    private void pullImage(String orgName, String imageName, String version) {
+        executor.execute(() -> {
+            try {
+                Process process = Runtime.getRuntime()
+                        .exec(String.format(Constants.CELLERY_PULL_COMMAND, orgName, imageName, version));
+                int exitCode = process.waitFor();
+                if (exitCode == 0) {
+                    initializeImageFromLocalRepo(orgName, imageName, version);
+                } else {
+                    logger.error("Failed to pull Cellery Image with exit code " + exitCode);
+                }
+            } catch (IOException | InterruptedException e) {
+                logger.error("Failed to fetch image " + getImageFQN(orgName, imageName, version), e);
+            }
+        });
+    }
+
+    /**
+     * Initialize an image in the local repo.
+     *
+     * Relevant information will be extracted from the image as well.
+     *
+     * @param orgName The name of the organization the image belongs to
+     * @param imageName The name of the image
+     * @param version The version of the image
+     * @return The image from from the local repository with information extracted
+     */
+    private synchronized Image initializeImageFromLocalRepo(String orgName, String imageName, String version) {
+        Image image = new Image(orgName, imageName, version);
+        image.extractInformation();
+        images.put(getImageFQN(orgName, imageName, version), image);
+        return image;
+    }
+
+    /**
+     * Get the fully qualified name of an image
+     *
+     * @param orgName The name of the organization the image belongs to
+     * @param imageName The name of the image
+     * @param version The version of the image
+     * @return Image fully qualified name
+     */
+    private static String getImageFQN(String orgName, String imageName, String version) {
+        return orgName + "/" + imageName + ":" + version;
     }
 
     /**
@@ -77,6 +203,7 @@ public class ImageManager {
      */
     public static class Image {
         private static final Logger logger = LoggerFactory.getLogger(Image.class);
+        private static final Gson gson = new Gson();
         private static final Type referenceTypeToken = new ReferenceTypeToken().getType();
 
         private String org;
@@ -94,25 +221,18 @@ public class ImageManager {
             this.imageFile = new File(Constants.LOCAL_REPO_DIRECTORY + File.separator + orgName
                     + File.separator + imageName + File.separator + version + File.separator
                     + imageName + Constants.CELLERY_IMAGE_EXTENSION);
-
-            // Collecting image information (pulling the image from Cellery Hub if necessary)
-            if (imageFile.exists()) {
-                extractInformationFromImage();
-            } else {
-                pullImage(orgName, imageName, version);
-            }
         }
 
         public synchronized Map<String, String> getReferenceKeys() {
             return referenceKeys;
         }
 
-        public synchronized byte[] getLastKnownDigest() {
-            return lastKnownDigest.clone();
+        public String getFQN() {
+            return ImageManager.getImageFQN(org, name, version);
         }
 
-        public String getFQN() {
-            return org + "/" + name + ":" + version;
+        private synchronized byte[] getLastKnownDigest() {
+            return lastKnownDigest;
         }
 
         /**
@@ -121,44 +241,22 @@ public class ImageManager {
          * @return The digest of the Cellery Image
          */
         private byte[] getCurrentDigest() {
-            byte[] digest;
-            try {
-                MessageDigest md5 = MessageDigest.getInstance("MD5");
-                digest = md5.digest(IOUtils.toByteArray(new FileInputStream(imageFile)));
-            } catch (IOException | NoSuchAlgorithmException e) {
-                digest = new byte[0];
+            byte[] digest = null;
+            if (imageFile.exists()) {
+                try {
+                    MessageDigest md5 = MessageDigest.getInstance("MD5");
+                    digest = md5.digest(IOUtils.toByteArray(new FileInputStream(imageFile)));
+                } catch (IOException | NoSuchAlgorithmException e) {
+                    digest = null;
+                }
             }
             return digest;
         }
 
         /**
-         * Pull an image from the Registry.
-         *
-         * @param orgName The name of the organization the image belogns to
-         * @param imageName The name of the image
-         * @param version The version of the image
-         */
-        private void pullImage(String orgName, String imageName, String version) {
-            executor.execute(() -> {
-                try {
-                    Process process = Runtime.getRuntime()
-                            .exec(String.format(Constants.CELLERY_PULL_COMMAND, orgName, imageName, version));
-                    int exitCode = process.waitFor();
-                    if (exitCode == 0) {
-                        extractInformationFromImage();
-                    } else {
-                        logger.error("Failed to pull Cellery Image with exit code " + exitCode);
-                    }
-                } catch (IOException | InterruptedException e) {
-                    logger.error("Failed to fetch image " + orgName + "/" + imageName + ":" + version, e);
-                }
-            });
-        }
-
-        /**
          * Extract information from the image.
          */
-        private synchronized void extractInformationFromImage() {
+        private synchronized void extractInformation() {
             try (ZipFile celleryImageZip = new ZipFile(imageFile)) {
                 ZipEntry zipEntry = celleryImageZip.getEntry(Constants.CELLERY_IMAGE_REFERENCE_FILE);
                 String referenceJsonString = IOUtils.toString(celleryImageZip.getInputStream(zipEntry),
